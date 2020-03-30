a6935745ec1e36c6191ca2e4303e934d00056bb2
tknack <tknack66@gmail.com>
2020-03-30 19:49:23 +0200

    broken our homepage into smaller component, finished styling our menu-item

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react' 
+import PropTypes from 'prop-types'
+
+import Directory from '../../components/directory/directory.component'
+
+import './homepage.styles.scss'
+
+const HomePage = props => {
+  return (
+    <div className='homepage'>
+      <Directory />
+    </div>
+  )
+}
+
+HomePage.propTypes = {
+
+}
+
+export default HomePage
