7cade2158847e0c6e4be459b4eea5024d099dccd
tknack <tknack66@gmail.com>
2020-04-08 19:28:45 +0200

    added cartIcon and cartDropdown to application, also implemented reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,18 +5,19 @@ import PropTypes from 'prop-types'
 
 import { auth } from '../../firebase/firebase.utils'
 import CartIcon from '../cart-icon/cart-icon.component'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import './header.styles.scss'
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
   return (
     <div className='header'>
       <Link className='logo-container' to='/'>
         <Logo className='logo' />
       </Link>
       <div className='options'>
-        <Link className='option' to='/shop'> 
+        <Link className='option' to='/shop'>
           SHOP
         </Link>
         <Link className='option' to='/shop'>
@@ -33,14 +34,16 @@ const Header = ({ currentUser }) => {
         )}
         <CartIcon />
       </div>
+      {hidden ? null : <CartDropdown />}
     </div>
   )
 }
 
 Header.propTypes = {}
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 })
 
 export default connect(mapStateToProps)(Header)
